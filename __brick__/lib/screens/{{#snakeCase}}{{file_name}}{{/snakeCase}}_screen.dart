9e048cf775b618a75c60ad454e558ab84c79f4a3
nico-develop <contact@nico-develop.fr>
2022-09-18 19:18:24 +0200

    Permet l'intégration d'une vue gestion de profil

--- __brick__/lib/screens/{{#snakeCase}}{{file_name}}{{/snakeCase}}_screen.dart
+++ __brick__/lib/screens/{{#snakeCase}}{{file_name}}{{/snakeCase}}_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:settings_screen/settings_screen.dart';
-import './about_screen.dart';
+import 'package:{{file_name}}_screen/{{file_name}}_screen.dart';
+{{#about_screen}}import './about_screen.dart';{{/about_screen}}
+{{#profile_screen}}import './profile_screen.dart';{{/profile_screen}}
 
 class {{file_name.pascalCase()}}Screen extends StatelessWidget {
   const {{file_name.pascalCase()}}Screen({Key? key}) : super(key: key);
@@ -14,15 +15,19 @@ class {{file_name.pascalCase()}}Screen extends StatelessWidget {
         ),
       ),
       body: settingsScreen(
-        [{{#about_screen}}
-          {
+        [{{#profile_screen}}{
+            "title": "Profile",
+            "subtitle": "Edit your profile",
+            "onTap": () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const ProfileScreen())),
+          },{{/profile_screen}}
+          {{#about_screen}}{
             "title": "About",
             "subtitle": "Version 1.0.0",
             "onTap": () => Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const AboutScreen())),
           },{{/about_screen}}
-        ],
-        // optional
+        ]
       ),
     );
   }
